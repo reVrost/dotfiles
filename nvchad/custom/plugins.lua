4a78077472b7abb88b11430c0c94c294e153a7a7
Kenley Bastari <kenley.bastari@immutable.com>
2024-02-15 22:32:01 +1100

    feat: plugins update added gitdiff

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -1,5 +1,9 @@
 local plugins = {
    {
+      "sindrets/diffview.nvim",
+      lazy = true,
+   },
+   {
       "echasnovski/mini.files",
       version = false,
       lazy = false,
@@ -237,6 +241,43 @@ local plugins = {
                   ["<Leader>A"] = "@parameter.inner",
                },
             },
+            move = {
+               enable = true,
+               set_jumps = true,
+               goto_next_start = {
+                  ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+                  ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+                  ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                  ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+                  ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+
+                  -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+                  -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+                  ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+                  ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+               },
+               goto_next_end = {
+                  ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+                  ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+                  ["]C"] = { query = "@class.outer", desc = "Next class end" },
+                  ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+                  ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+               },
+               goto_previous_start = {
+                  ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+                  ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+                  ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+                  ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+                  ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+               },
+               goto_previous_end = {
+                  ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+                  ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+                  ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+                  ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+                  ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+               },
+            },
             lsp_interop = {
                enable = true,
             },
@@ -308,12 +349,6 @@ local plugins = {
          current_line_blame_formatter = "\t<author>, <author_time:%Y-%m-%d> - <summary>",
       },
    },
-   -- {
-   --    "sindrets/diffview.nvim",
-   --    after = "plenary.nvim",
-   --    requires = "nvim-lua/plenary.nvim",
-   --    lazy = true,
-   -- },
    -- ["reVrost/played.nvim"] is commented out in the original list
    {
       "folke/trouble.nvim",
