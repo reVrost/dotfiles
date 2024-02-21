c7d5034ae3e6b83d6295f14e221d3c3d0847a40a
Kenley Bastari <kenley.bastari@immutable.com>
2024-02-22 02:01:15 +1100

    feat: added hl color for cursorline

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -7,8 +7,10 @@ M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
 M.ui = {
-  theme = th,
-  -- hl_override = "custom.highlights",
+   theme = th,
+   hl_override = {
+      CursorLine = { bg = "#25252f" },
+   },
 }
 
 return M
