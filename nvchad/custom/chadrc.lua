eb2e46fcf0acb556d761a3fb7471c791c81bd2a2
Kenley Bastari <kenley.bastari@immutable.com>
2024-03-01 19:49:46 +1100

    added my aquarium theme

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -1,5 +1,4 @@
 local M = {}
-local th = "aquarium"
 
 -- Plugins
 M.plugins = "custom.plugins"
@@ -7,10 +6,47 @@ M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
 M.ui = {
-   theme = th,
-   hl_override = {
-      CursorLine = { bg = "#25252f" },
-   },
+	theme = "aquarium",
+	hl_override = {
+		CursorLine = { bg = "#25252f" },
+	},
+	theme_toggle = {
+		"aquarium",
+		"everforest",
+	},
+	changed_themes = {
+		mountain = {
+			base_16 = {
+				base00 = "#161620",
+			},
+		},
+
+		oxocarbon = {
+			base_30 = {
+				-- green = "#B5E8E0",
+			},
+
+			base_16 = {
+				base00 = "#0f0f0f", -- Darker
+				base01 = "#1f1f1f", -- Darker
+				base02 = "#303030", -- Darker
+				base03 = "#525252",
+				base04 = "#dde1e6",
+				base05 = "#f2f4f8",
+				base06 = "#ffffff",
+				base07 = "#08bdba",
+				base08 = "#3ddbd9",
+				base09 = "#78a9ff",
+				base0A = "#ee5396",
+				base0B = "#33b1ff",
+				base0C = "#ff7eb6",
+				-- base0D = "#42be65",
+				base0D = "#3ddbd9",
+				base0E = "#be95ff",
+				base0F = "#82cfff",
+			},
+		},
+	},
 }
 
 return M
--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -13,103 +13,106 @@ opt.splitkeep = "screen"
 g.vscode_snippets_path = fn.stdpath "config" .. "/lua/custom/snippets"
 g.lua_snippets_path = fn.stdpath "config" .. "/lua/custom/snippets"
 
+-- if using neovide
+g.neovide_scale_factor = 0.8
+
 -- WSL clipboard
 if vim.fn.has "wsl" == 1 then
-   g.clipboard = {
-      name = "WslClipboard",
-      copy = {
-         ["+"] = "clip.exe",
-         ["*"] = "clip.exe",
-      },
-      paste = {
-         ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
-         ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
-      },
-      cache_enabled = 0,
-   }
+	g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
+		},
+		cache_enabled = 0,
+	}
 end
 
 -- Global funcs, utilities
 local t = function(str)
-   return vim.api.nvim_replace_termcodes(str, true, true, true)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 _G.run_command = function()
-   local filetype = vim.bo.filetype
-   if filetype == "c" then
-      vim.cmd "!sh run.sh"
-   end
-   if filetype == "go" then
-      run_make_dev()
-   end
+	local filetype = vim.bo.filetype
+	if filetype == "c" then
+		vim.cmd "!sh run.sh"
+	end
+	if filetype == "go" then
+		run_make_dev()
+	end
 end
 
 -- specific to whatever you're working on atm
 _G.run_make_dev = function()
-   -- Find the directory containing the Makefile
-   local makefile_path = vim.fn.findfile("Makefile", ".")
-   print "Rebuilding..."
+	-- Find the directory containing the Makefile
+	local makefile_path = vim.fn.findfile("Makefile", ".")
+	print "Rebuilding..."
 
-   -- Check if Makefile is found
-   if makefile_path ~= "" then
-      -- Change to the directory containing the Makefile
-      local makefile_dir = vim.fn.fnamemodify(makefile_path, ":h")
-      vim.cmd("cd " .. makefile_dir)
+	-- Check if Makefile is found
+	if makefile_path ~= "" then
+		-- Change to the directory containing the Makefile
+		local makefile_dir = vim.fn.fnamemodify(makefile_path, ":h")
+		vim.cmd("cd " .. makefile_dir)
 
-      -- Kill previous process if running
-      vim.fn.system "pkill -f marketpal_server"
+		-- Kill previous process if running
+		vim.fn.system "pkill -f marketpal_server"
 
-      -- Run make dev
-      vim.fn.jobstart("make dev", { detach = true })
-      print "Dev is running."
-   else
-      print "Makefile not found."
-   end
+		-- Run make dev
+		vim.fn.jobstart("make dev", { detach = true })
+		print "Dev is running."
+	else
+		print "Makefile not found."
+	end
 end
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-   group = highlight_group,
-   callback = function()
-      vim.highlight.on_yank()
-   end,
+	group = highlight_group,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.api.nvim_create_user_command("DiffviewToggle", function(e)
-   local view = require("diffview.lib").get_current_view()
+	local view = require("diffview.lib").get_current_view()
 
-   if view then
-      vim.cmd "DiffviewClose"
-   else
-      vim.cmd("DiffviewOpen " .. e.args)
-   end
+	if view then
+		vim.cmd "DiffviewClose"
+	else
+		vim.cmd("DiffviewOpen " .. e.args)
+	end
 end, { nargs = "*" })
 
 _G.snip_complete = function()
-   if fn.pumvisible() == 1 then
-      return t "<C-n>"
-   elseif require("luasnip").expand_or_jumpable() then
-      return t "<Plug>luasnip-expand-or-jump"
-   end
-   return ""
+	if fn.pumvisible() == 1 then
+		return t "<C-n>"
+	elseif require("luasnip").expand_or_jumpable() then
+		return t "<Plug>luasnip-expand-or-jump"
+	end
+	return ""
 end
 
 _G.r_snip_complete = function()
-   if fn.pumvisible() == 1 then
-      return t "<C-p>"
-   elseif require("luasnip").jumpable(-1) then
-      return t "<Plug>luasnip-jump-prev"
-   end
-   return ""
+	if fn.pumvisible() == 1 then
+		return t "<C-p>"
+	elseif require("luasnip").jumpable(-1) then
+		return t "<Plug>luasnip-jump-prev"
+	end
+	return ""
 end
 
 function _G.smart_tab()
-   return require("luasnip").expand_or_jumpable() and t "<Plug>luasnip-expand-or-jump" or t "<Tab>"
+	return require("luasnip").expand_or_jumpable() and t "<Plug>luasnip-expand-or-jump" or t "<Tab>"
 end
 
 vim.keymap.set({ "n", "x", "o" }, "s", function()
-   local current_window = vim.fn.win_getid()
-   require("leap").leap { target_windows = { current_window } }
+	local current_window = vim.fn.win_getid()
+	require("leap").leap { target_windows = { current_window } }
 end)
