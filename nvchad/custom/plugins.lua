9ae650af028cfaa070498f674f810744d1910ffd
revrost <revrost@gmail.com>
2024-03-20 22:58:56 +1100

    feat: changed harpoon to grapple

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -239,8 +239,22 @@ local plugins = {
 		lazy = false,
 	},
 	{
-		"ThePrimeagen/harpoon",
-		lazy = false,
+		"cbochs/grapple.nvim",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+		opts = {
+			scope = "git_branch",
+			icons = true,
+			quick_select = "123456789",
+		},
+		keys = {
+			{ ";", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+
+			{ "<c-s>", "<cmd>Grapple toggle<cr>", desc = "Toggle tag" },
+			{ "H", "<cmd>Grapple cycle forward<cr>", desc = "Go to next tag" },
+			{ "L", "<cmd>Grapple cycle backward<cr>", desc = "Go to previous tag" },
+		},
 	},
 	{
 		"folke/which-key.nvim",
