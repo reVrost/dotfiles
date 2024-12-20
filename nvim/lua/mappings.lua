790f54e24ef57a56e8eab7ff8e2f9df37cbc5938
reVrost <revrost@gmail.com>
2024-12-20 16:13:04 +1100

    feat: fixed plugins

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -37,6 +37,20 @@ map("n", "<leader>lr", ":LspRestart<CR>", { desc = "LSP Restart" })
 map("n", "<leader>li", ":LspInfo<CR>", { desc = "LSP Info" })
 map("n", "<leader>ms", ":Mason<CR>", { desc = "Open Mason" })
 map("n", "<leader>e", ":lua MiniFiles.open()<CR>", { desc = "Open MiniFiles" })
+map("i", "<C-;>", function()
+	vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, {
+	desc = "Copilot Accept",
+	replace_keycodes = true,
+	nowait = true,
+	silent = true,
+	expr = true,
+	noremap = true,
+})
+map("n", "s", "<Plug>(leap)")
+map("n", "S", "<Plug>(leap-from-window)")
+map({ "x", "o" }, "s", "<Plug>(leap-forward)")
+map({ "x", "o" }, "S", "<Plug>(leap-backward)")
 
 -- LSP key mappings
 map("n", "<leader>ra", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "Rename symbol" })
@@ -54,46 +68,46 @@ map("n", "<leader>fmg", ":CellularAutomaton game_of_life<CR>", { desc = "Game of
 
 -- Telescope mappings
 map("n", "<leader>fq", function()
-  require("telescope.builtin").quickfix()
+	require("telescope.builtin").quickfix()
 end, { desc = "Telescope Quickfix" })
 map("n", "<leader>fj", function()
-  require("telescope.builtin").jumplist()
+	require("telescope.builtin").jumplist()
 end, { desc = "Telescope Jumplist" })
 map("n", "<leader>fr", function()
-  require("telescope.builtin").lsp_references()
+	require("telescope.builtin").lsp_references()
 end, { desc = "LSP References" })
 map("n", "<leader>fs", function()
-  require("telescope.builtin").lsp_workspace_symbols()
+	require("telescope.builtin").lsp_workspace_symbols()
 end, { desc = "Workspace Symbols" })
 map("n", "<leader>fc", function()
-  require("telescope.builtin").command_history()
+	require("telescope.builtin").command_history()
 end, { desc = "Command History" })
 map("n", "<leader>fi", function()
-  require("telescope.builtin").lsp_implementations()
+	require("telescope.builtin").lsp_implementations()
 end, { desc = "LSP Implementations" })
 map("n", "<leader>fv", function()
-  require("telescope.builtin").find_files({
-    shorten_path = false,
-    cwd = "~/.config/nvim/lua/custom",
-    prompt = "~ dotfiles ~",
-    hidden = true,
-    layout_strategy = "horizontal",
-    layout_config = { preview_width = 0.55 },
-    file_ignore_patterns = { "^node_modules/" },
-  })
+	require("telescope.builtin").find_files({
+		shorten_path = false,
+		cwd = "~/.config/nvim/lua/custom",
+		prompt = "~ dotfiles ~",
+		hidden = true,
+		layout_strategy = "horizontal",
+		layout_config = { preview_width = 0.55 },
+		file_ignore_patterns = { "^node_modules/" },
+	})
 end, { desc = "Find dotfiles" })
 map("n", "<leader>fw", function()
-  local lga_actions = require("telescope-live-grep-args.actions")
-  require("telescope").extensions.live_grep_args.live_grep_args({
-    mappings = {
-      i = {
-        ["<C-k>"] = lga_actions.quote_prompt({ postfix = " -t" }),
-        ["<C-g>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-      },
-    },
-    hidden = true,
-    layout_strategy = "horizontal",
-    layout_config = { preview_width = 0.55 },
-    file_ignore_patterns = { "^node_modules/" },
-  })
+	local lga_actions = require("telescope-live-grep-args.actions")
+	require("telescope").extensions.live_grep_args.live_grep_args({
+		mappings = {
+			i = {
+				["<C-k>"] = lga_actions.quote_prompt({ postfix = " -t" }),
+				["<C-g>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+			},
+		},
+		hidden = true,
+		layout_strategy = "horizontal",
+		layout_config = { preview_width = 0.55 },
+		file_ignore_patterns = { "^node_modules/" },
+	})
 end, { desc = "Live grep with Telescope" })
