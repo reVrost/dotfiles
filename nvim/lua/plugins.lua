42a7188fc2868ee790a608a79dfc6255e7e0bdb2
Kenley Bastari <kenley.bastari@immutable.com>
2024-12-22 18:07:02 +1100

    feat: setup avante

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -473,6 +473,57 @@ local plugins = {
       vim.keymap.set("n", "<leader>b", '<cmd>lua require("dropbar.api").pick()<cr>', { desc = "dropbar: pick" })
       require("dropbar").setup(opts)
     end,
+    {
+      "yetone/avante.nvim",
+      event = "VeryLazy",
+      lazy = false,
+      version = false, -- set this if you want to always pull the latest change
+      opts = {
+        -- add any opts here
+      },
+      keys = {
+        { "<leader>aa", "<cmd>AvanteAsk<CR>", desc = "avante: open" },
+        { "<leader>ac", "<cmd>AvanteChat<CR>", desc = "avante: chat" },
+        { "<leader>ae", "<cmd>AvanteEdit<CR>", desc = "avante: edit" },
+      },
+      -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+      build = "make",
+      -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+      dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
+          -- support for image pasting
+          "HakonHarnes/img-clip.nvim",
+          event = "VeryLazy",
+          opts = {
+            -- recommended settings
+            default = {
+              embed_image_as_base64 = false,
+              prompt_for_file_name = false,
+              drag_and_drop = {
+                insert_mode = true,
+              },
+              -- required for Windows users
+              use_absolute_path = true,
+            },
+          },
+        },
+        {
+          -- Make sure to set this up properly if you have lazy=true
+          "MeanderingProgrammer/render-markdown.nvim",
+          opts = {
+            file_types = { "markdown", "Avante" },
+          },
+          ft = { "markdown", "Avante" },
+        },
+      },
+    },
   },
   -- {
   --   "karb94/neoscroll.nvim",
