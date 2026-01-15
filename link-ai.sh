5effd669740715a0163a70d96b85916deb6974f3
reVrost <revrost@gmail.com>
2026-01-15 12:54:43 +1100

    feat: added skills

--- link-ai.sh
+++ link-ai.sh
@@ -4,6 +4,9 @@
 
 mkdir -p "$HOME/.claude"
 ln -s $(pwd)/commands "$HOME/.claude/commands"
+ln -s $(pwd)/skills "$HOME/.claude/skills"
 
 mkdir -p "$HOME/.config/crush"
 ln -s $(pwd)/commands "$HOME/.config/crush/commands"
+ln -s $(pwd)/skills "$HOME/.config/crush/skills"
+
